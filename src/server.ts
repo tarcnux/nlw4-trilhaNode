b42c0e6ab5555c39d0307c8b2ba05eda7acea6c8
Tarcisio Nunes <github@tarcnux.com.br>
2021-02-25 00:41:11 -0300

    Criar rotas GEt e POST

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
+
+app.get("/", (request, response) => {
+    return response.json({message: "Alo HA NLW#4 fevereiro de 2021"});
+});
+
+app.post("/", (request, response) => {
+    return response.json({message: "Alo HA POST"});
+});
 
 app.listen(3333, () => console.log('Server is running on Port: 3333'));
